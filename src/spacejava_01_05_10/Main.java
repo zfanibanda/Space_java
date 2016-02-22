68ec1be926c588908d66fc2c86a26781e8b9c70e
zfaniban <zfaniban@hawk.iit.edu>
2016-02-22 10:57:37 -0600

    just checking again

--- src/spacejava_01_05_10/Main.java
+++ src/spacejava_01_05_10/Main.java
@@ -37,7 +37,7 @@ public class Main {
         
         private static final Logger logger = Logger.getLogger("spacejava_01_05_10.Main");
         static Scanner user_input = new Scanner(System.in);
-        System.out.println("Hello Fanibanda");
+        
 
     public static void main(String[] args) throws IOException {
       
